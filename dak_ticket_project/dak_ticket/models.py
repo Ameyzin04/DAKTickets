08879d06ab293e92eb1da73a8eacb5a9451396cb
kevin <xinjiewang2882@gmail.com>
2024-03-25 14:38:44 -0400

    backend initial set up

---
+++ dak_ticket_project/dak_ticket/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+
+class Venue(models.Model):
+    name = models.CharField(max_length=100)
+    address = models.CharField(max_length=100)
+    state = models.CharField(max_length=100)
+    city = models.CharField(max_length=100)
+    zip_code = models.CharField(max_length=100)
+    capacity = models.IntegerField()
+    photo_url = models.URLField()
+
+    def __str__(self):
+        return self.name
+
+class Event(models.Model):
+    EVENT_TYPES = [
+        ('sports', 'Sports'),
+        ('concerts', 'Concerts'),
+    ]
+    name = models.CharField(max_length=100)
+    time = models.CharField(max_length=100)
+    type = models.CharField(max_length=100, choices=EVENT_TYPES)
+    duration = models.CharField(max_length=100)
+    photo_url = models.URLField()
+    venue = models.ForeignKey(Venue, on_delete=models.CASCADE, null=True)
+
+    def __str__(self):
+        return self.name
