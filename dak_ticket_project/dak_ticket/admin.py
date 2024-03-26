315dbed526cbd2916554af45c15aec2a93cb5b62
kevin <xinjiewang2882@gmail.com>
2024-03-26 12:06:08 -0400

    not working userlogin

--- dak_ticket_project/dak_ticket/admin.py
+++ dak_ticket_project/dak_ticket/admin.py
@@ -1,7 +1,9 @@
-from django.contrib import admin
+
 
 # Register your models here.
 from django.contrib import admin
-from .models import Event, Venue
+from .models import Event, Venue,CustomUser
+
 admin.site.register(Event)
 admin.site.register(Venue)
+admin.site.register(CustomUser)
--- dak_ticket_project/dak_ticket/views.py
+++ dak_ticket_project/dak_ticket/views.py
@@ -1,9 +1,40 @@
-from django.shortcuts import render
-
+from django.contrib.auth import authenticate, login, logout
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 # Create your views here.
 from rest_framework import generics
 from .serializers import EventSerializer,VenueSerializer
-from .models import Event,Venue
+from .models import Event,Venue,CustomUser
+
+
+@api_view(['POST'])
+def user_login(request):
+    email = request.data.get('email')
+    password = request.data.get('password')
+    user = authenticate(request, email=email, password=password)
+    if user is not None:
+        login(request, user)
+        return Response({'message': 'Login successful'})
+    else:
+        return Response({'message': 'Invalid credentials'}, status=401)
+
+@api_view(['POST'])
+def user_signup(request):
+    email = request.data.get('email')
+    password = request.data.get('password')
+    first_name = request.data.get('first_name')
+    last_name = request.data.get('last_name')
+    user = CustomUser.objects.create_user(email=email, password=password, first_name=first_name, last_name=last_name)
+    if user is not None:
+        return Response({'message': 'Signup successful'})
+    else:
+        return Response({'message': 'Signup failed'}, status=400)
+
+@api_view(['POST'])
+def user_logout(request):
+    logout(request)
+    return Response({'message': 'Logout successful'})
+
 
 class EventList(generics.ListCreateAPIView):
     queryset = Event.objects.all()
@@ -20,3 +51,4 @@ class VenueList(generics.ListCreateAPIView):
 class VenueDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Venue.objects.all()
     serializer_class = VenueSerializer
+
