1168fbbe8996eb379b6b08047b1717f11214a0e0
kevin <xinjiewang2882@gmail.com>
2024-03-27 10:06:11 -0400

    dd

--- front_end/dak_tickets_front_end/src/components/Header.jsx
+++ front_end/dak_tickets_front_end/src/components/Header.jsx
@@ -1,3 +1,33 @@
-export default function Header(){
-    
+import Nav from './Nav'
+// import '../styles/Template.css'
+
+export default function Header() {
+    return (
+        <div className="header-container">
+            <div className="logo-container">
+                {/* <h1>DAK TICKETS</h1> */}
+                <img className="active-member animations" src='/src/assets/Active-Members.png' style={{ width: '150px', height: '100px', border: 'none' }}></img>
+                <img className="logo-iframe animations" src="/src/assets/DAK-Logo-A-1.gif"
+                    // src="/src/assets/Logo.gif" 
+                    style={{ width: '150px', height: '100px', border: 'none' }} 
+                    title="Project Logo" 
+                />
+                <img className="promotion animations" src='/src/assets/Promotion.gif' style={{ width: '150px', height: '100px', border: 'none' }} alt="" />
+            </div>
+            <div className="nav-container">
+                <Nav />
+            </div>
+            <div className="search-container">
+            <input className='home-search-inputs' 
+                    id='home_Search_inputs'
+                   type="text"
+                   placeholder='Seacrh by Venue, Sports Team or Artist'  />
+            <button className='search-button'
+                    id='home_search_button'
+                    type="button">
+                    SEARCH</button>
+              
+            </div>
+        </div>
+    )
 }
--- front_end/dak_tickets_front_end/src/components/Nav.jsx
+++ front_end/dak_tickets_front_end/src/components/Nav.jsx
@@ -1,3 +1,20 @@
-export default function Nav(){
-    
+import { Link } from 'react-router-dom'
+// import '../styles/Template.css'
+export default function Nav() {
+
+    return (
+        <div className="nav-container">
+            <div className='left-nav-panel'>
+                <Link to='/'>Home</Link>
+                <Link to='/concert'>Concert</Link>
+                <Link to='/sport'>Sport</Link>
+            </div>
+            <div className='right-nav-panel'>
+                <img className='userlogin-logo' src='./src/assets/userlogin.svg'></img>
+                <Link to='/login'>Login</Link>/
+                <Link to='/registration'>Registration</Link>
+            </div>
+
+        </div>
+    )
 }
