e99e6ce0412ca1a02104e3404dc7df65ebc18b62
Denesh Anandathasan <denesh555@gmail.com>
2024-03-27 09:41:13 -0400

    Pull commit

--- front_end/dak_tickets_front_end/src/components/Header.jsx
+++ front_end/dak_tickets_front_end/src/components/Header.jsx
@@ -5,14 +5,14 @@ export default function Header() {
     return (
         <div className="header-container">
             <div className="logo-container">
-                <h1>DAK TICKETS</h1>
-            {/* <img
-                    className="logo-iframe"
-                    src="https://www.canva.com/design/DAGAocdasWg/XEohiKzAI9A52LymudMa3w/watch?utm_content=DAGAocdasWg&utm_campaign=designshare&utm_medium=link&utm_source=editor"
-                    style={{ width: '100px', height: '100px', border: 'none' }} // Example of inline style as an object
-                    title="Project Logo" // Adding a title for accessibility
-                /> */}
-                {/* <iframe class="logo-iframe" src="https://www.canva.com/design/your_design_code/embed?utm_content=your_design_code&utm_campaign=designshare&utm_medium=embeds&utm_source=link" style="width:1024px;height:768px;"></iframe> */}
+                {/* <h1>DAK TICKETS</h1> */}
+                <img className="active-member animations" src='/src/assets/Active-Members.png' style={{ width: '150px', height: '100px', border: 'none' }}></img>
+                <img className="logo-iframe animations" src="/src/assets/DAK-Logo-A-1.gif"
+                    // src="/src/assets/Logo.gif" 
+                    style={{ width: '150px', height: '100px', border: 'none' }} 
+                    title="Project Logo" 
+                />
+                <img className="promotion animations" src='/src/assets/Promotion.gif' style={{ width: '150px', height: '100px', border: 'none' }} alt="" />
             </div>
             <div className="nav-container">
                 <Nav />
