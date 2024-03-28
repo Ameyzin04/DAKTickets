9a7e2a9432c282844c197b227419ed186421bfba
Denesh Anandathasan <denesh555@gmail.com>
2024-03-27 21:57:30 -0400

    search result page is completed : 90%

--- dak_ticket_project/dak_ticket/serializers.py
+++ dak_ticket_project/dak_ticket/serializers.py
@@ -8,17 +8,29 @@ class EventSerializer(serializers.HyperlinkedModelSerializer):
         read_only=True  # Assuming you don't want to allow writing to the venue field directly
     )
 
+    venue_name = serializers.CharField(
+        read_only=True,
+        source='venue.name'
+    )
+    
+    event_url = serializers.ModelSerializer.serializer_url_field(
+    view_name='event_detail'
+    )
+    
+        
     class Meta:
         model = Event
-        fields = ('id', 'name', 'time', 'type', 'duration', 'photo_url','venue' )
+        fields = ('id', 'name', 'time', 'type', 'duration', 'photo_url','event_url','venue','venue_name')
+
 
 class VenueSerializer(serializers.HyperlinkedModelSerializer):
-    event = serializers.HyperlinkedRelatedField(
-        view_name='event_detail',  # Assuming this is the name of your event detail URL pattern
+    events = EventSerializer(
+        # view_name='event_detail',  # Assuming this is the name of your event detail URL pattern
         many=True,
         read_only=True  # Assuming you don't want to allow writing to the event field directly
     )
 
+
     class Meta:
         model = Venue
-        fields = ('id', 'name', 'address', 'state', 'city', 'zip_code', 'capacity', 'photo_url','event')
+        fields = ('id', 'name', 'address', 'state', 'city', 'zip_code', 'capacity', 'photo_url','events',)
