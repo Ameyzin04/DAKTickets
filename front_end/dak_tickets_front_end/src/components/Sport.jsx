4948777f5008ae5374146e30ea1558ee9c1c4ab3
Denesh Anandathasan <denesh555@gmail.com>
2024-03-28 09:30:53 -0400

    TEste commit

--- front_end/dak_tickets_front_end/src/components/Sport.jsx
+++ front_end/dak_tickets_front_end/src/components/Sport.jsx
@@ -1,5 +1,44 @@
-export default function Sport(){
+import { useEffect, useState } from "react"
+import { useParams } from "react-router-dom"
+import axios from "axios";
+
+export default function Sport () {
+    let {sportId} = useParams()
+    const [sport , setSport] = useState ([])
+    useEffect(() => {
+        const getSport = async () => {
+
+        
+        try {
+            const response = await axios.get(`http://localhost:8000/api/event`)
+            console.log(respnose.data)
+            setSport (response.data)
+            console.log(sport)
+        } catch (e) {
+            console.error("error fetching", e)
+        }
+    }
+
+    getSport()
+    }, [sportId])
     return(
-        <><h1>I am Sports list apage</h1></>
-    )   
+        <div className='sport'>
+            <h1>All Sports events near you</h1>
+            {sport.map((sport)=>{
+                return(
+                    <div>
+                        <img src={sport.img_path} alt=""/>
+                        <h2>{sport.time}</h2>
+                        <h2>{sport.nanme}</h2>
+                        <h3>{sport.venue}</h3>
+                    </div>
+
+                )
+            }
+            )}
+        </div>
+    )
+
+  
+    
 }
